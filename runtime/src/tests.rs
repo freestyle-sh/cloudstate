54ecf6f468ff3df4ea1aeaea1ba5ddab979630c4
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2024-08-28 00:25:31 -0700

    new: map_empty_set

--- runtime/src/tests.rs
+++ runtime/src/tests.rs
@@ -19,6 +19,7 @@ js_test!(fetch);
 js_test!(get_cloudstate);
 js_test!(map_clear);
 js_test!(map_delete);
+js_test!(map_empty_set);
 js_test!(map_entries);
 js_test!(map_keys);
 js_test!(map_of_objects);
