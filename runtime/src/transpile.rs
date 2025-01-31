17bdebf184ac14d6a730831eb5c92586c7a9550b
Will Hopkins <willothyh@gmail.com>
2025-01-30 18:38:34 -0800

    style: run `cargo fmt`

--- runtime/src/transpile.rs
+++ runtime/src/transpile.rs
@@ -11,57 +11,57 @@ use deno_error::JsErrorBox;
 use std::path::Path;
 
 pub fn maybe_transpile_source(
-  name: ModuleName,
-  source: ModuleCodeString,
+    name: ModuleName,
+    source: ModuleCodeString,
 ) -> Result<(ModuleCodeString, Option<SourceMapData>), deno_error::JsErrorBox> {
-  // Always transpile `node:` built-in modules, since they might be TypeScript.
-  let media_type = if name.starts_with("node:") {
-    MediaType::TypeScript
-  } else {
-    MediaType::from_path(Path::new(&name))
-  };
+    // Always transpile `node:` built-in modules, since they might be TypeScript.
+    let media_type = if name.starts_with("node:") {
+        MediaType::TypeScript
+    } else {
+        MediaType::from_path(Path::new(&name))
+    };
 
-  match media_type {
-    MediaType::TypeScript => {}
-    MediaType::JavaScript => return Ok((source, None)),
-    MediaType::Mjs => return Ok((source, None)),
-    _ => panic!(
-      "Unsupported media type for snapshotting {media_type:?} for file {}",
-      name
-    ),
-  }
+    match media_type {
+        MediaType::TypeScript => {}
+        MediaType::JavaScript => return Ok((source, None)),
+        MediaType::Mjs => return Ok((source, None)),
+        _ => panic!(
+            "Unsupported media type for snapshotting {media_type:?} for file {}",
+            name
+        ),
+    }
 
-  let parsed = deno_ast::parse_module(ParseParams {
-    specifier: deno_core::url::Url::parse(&name).unwrap(),
-    text: source.into(),
-    media_type,
-    capture_tokens: false,
-    scope_analysis: false,
-    maybe_syntax: None,
-  })
-  .map_err(|err| JsErrorBox::generic(err.to_string()))?;
+    let parsed = deno_ast::parse_module(ParseParams {
+        specifier: deno_core::url::Url::parse(&name).unwrap(),
+        text: source.into(),
+        media_type,
+        capture_tokens: false,
+        scope_analysis: false,
+        maybe_syntax: None,
+    })
+    .map_err(|err| JsErrorBox::generic(err.to_string()))?;
 
-  let transpiled_source = parsed
-    .transpile(
-      &deno_ast::TranspileOptions {
-        imports_not_used_as_values: deno_ast::ImportsNotUsedAsValues::Remove,
-        ..Default::default()
-      },
-      &TranspileModuleOptions::default(),
-      &deno_ast::EmitOptions {
-        source_map: if cfg!(debug_assertions) {
-          SourceMapOption::Separate
-        } else {
-          SourceMapOption::None
-        },
-        ..Default::default()
-      },
-    )
-    .map_err(|err| deno_error::JsErrorBox::generic(err.to_string()))?
-    .into_source();
+    let transpiled_source = parsed
+        .transpile(
+            &deno_ast::TranspileOptions {
+                imports_not_used_as_values: deno_ast::ImportsNotUsedAsValues::Remove,
+                ..Default::default()
+            },
+            &TranspileModuleOptions::default(),
+            &deno_ast::EmitOptions {
+                source_map: if cfg!(debug_assertions) {
+                    SourceMapOption::Separate
+                } else {
+                    SourceMapOption::None
+                },
+                ..Default::default()
+            },
+        )
+        .map_err(|err| deno_error::JsErrorBox::generic(err.to_string()))?
+        .into_source();
 
-  // let maybe_source_map: Option<SourceMapData> = transpiled_source.source_map.map(|sm| Cow::from(sm).into());
-  // let source_text = String::from_utf8(transpiled_source)?;
+    // let maybe_source_map: Option<SourceMapData> = transpiled_source.source_map.map(|sm| Cow::from(sm).into());
+    // let source_text = String::from_utf8(transpiled_source)?;
 
-  Ok((transpiled_source.text.into(), None))
+    Ok((transpiled_source.text.into(), None))
 }
